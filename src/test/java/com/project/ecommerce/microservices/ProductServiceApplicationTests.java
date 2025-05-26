77e841f2229eb0a8c3027ac801f47f1954c15bb7
subramanivasu <subramaniyanvasu@gmail.com>
2025-05-26 23:12:38 +0530

    Added an Integration Test for Create Product with MongoDBContainer

--- src/test/java/com/project/ecommerce/microservices/ProductServiceApplicationTests.java
+++ src/test/java/com/project/ecommerce/microservices/ProductServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.project.ecommerce.microservices;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI= "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				    "name":"LG OLED TV",
+				    "description": "LG OLED TV is a high-end television with stunning picture quality",
+				    "price": 65000.0
+				}
+				""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("name", Matchers.equalTo("LG OLED TV"))
+				.body("description", Matchers.equalTo("LG OLED TV is a high-end television with stunning picture quality"))
+				.body("price", Matchers.equalTo(65000.0F));
+
+
+
 	}
 
 }
